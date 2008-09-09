98e2c26eb6d72dc9ebd7e8963ac77ad1955c04e1
Nathaniel Bibler <git@nathanielbibler.com>
2008-09-08 23:21:08 -0400

    Added check for Shoulda

--- init.rb
+++ init.rb
@@ -1,1 +1,7 @@
+begin
+  require 'shoulda'
+rescue LoadError
+  raise(LoadError, "Please install the Shoulda plugin from GitHub (http://github.com/thoughtbot/shoulda)")
+end
+
 Test::Unit::TestCase.send(:extend, ShouldRequireLogin)
